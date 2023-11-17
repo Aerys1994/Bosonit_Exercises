457a72e8cf772c68307af23a241cde25ec46a3f9
Aerys <andreu.aus.nz@gmail.com>
2023-11-17 14:29:40 +0100

    Block3CommandLineRunnerApp

---
+++ block5-command-line-runner/src/main/java/Components/AppConfig.java
@@ -0,0 +1,21 @@
+package Components;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public CommandLineRunner secundaria() {
+        return args -> System.out.println("Hola desde clase secundaria");
+    }
+
+    @Bean
+    public CommandLineRunner tercera() {
+        return args -> System.out.println("Soy la tercera clase");
+    }
+
+}
