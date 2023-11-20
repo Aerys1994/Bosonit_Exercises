06170c1229d9553b4df7347a0b5111864a4ef06a
Aerys <andreu.aus.nz@gmail.com>
2023-11-20 13:19:43 +0100

    Block 5 Properties fix

--- block5-properties/src/main/java/com/Bosonit/block5properties/MyProperties.java
+++ block5-properties/src/main/java/com/Bosonit/block5properties/MyProperties.java
@@ -1,23 +1,62 @@
 package com.Bosonit.block5properties;
 
 import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-@Component
+
+@Configuration
+@ConfigurationProperties(prefix = "my")
 public class MyProperties {
 
-    @Value("${greeting}")
     private String greeting;
 
-    @Value("${my.number}")
-    private int myNumber;
+    private int number;
 
     @Value("${new.property:no tiene valor}")
     private String newProperty;
 
+    @Value("${MYURL:default-url}")
+    private String myUrl;
+
     public void printProperties() {
         System.out.println("El valor de greeting es: " + greeting);
-        System.out.println("El valor de my.number es: " + myNumber);
+        System.out.println("El valor de my.number es: " + number);
         System.out.println("El valor de new.property es: " + newProperty);
+        System.out.println("MYURL from environment: " + myUrl);
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getNewProperty() {
+        return newProperty;
+    }
+
+    public void setNewProperty(String newProperty) {
+        this.newProperty = newProperty;
+    }
+
+    public String getMyUrl() {
+        return myUrl;
+    }
+
+    public void setMyUrl(String myUrl) {
+        this.myUrl = myUrl;
     }
 }
