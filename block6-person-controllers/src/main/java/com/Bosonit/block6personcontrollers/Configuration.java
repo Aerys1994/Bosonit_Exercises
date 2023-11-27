d89aa93daa5aa986de7f506d1209698ae64b4a2f
Aerys <andreu.aus.nz@gmail.com>
2023-11-27 13:20:31 +0100

    Block6 Person Controllers

--- block6-person-controllers/src/main/java/com/Bosonit/block6personcontrollers/Configuration.java
+++ block6-person-controllers/src/main/java/com/Bosonit/block6personcontrollers/Configuration.java
@@ -8,4 +8,25 @@ public class Configuration {
     public CiudadService ciudadService() {
         return new CiudadServiceImpl();
     }
+
+    @Bean("bean1")
+    public Person personBean1() {
+        Person person = new Person();
+        person.setNombre("bean1");
+        return person;
+    }
+
+    @Bean("bean2")
+    public Person personBean2() {
+        Person person = new Person();
+        person.setNombre("bean2");
+        return person;
+    }
+
+    @Bean("bean3")
+    public Person personBean3() {
+        Person person = new Person();
+        person.setNombre("bean3");
+        return person;
+    }
 }
