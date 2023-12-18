cf9b37a14c190ddffc058ff8fb3ba79dcaceae66
Aerys <andreu.aus.nz@gmail.com>
2023-12-18 09:11:13 +0100

    Block7 CRUD validation ex 3 (fixes needed)

---
+++ Block7-CRUD-validation/src/main/java/com/bosonit/Block7/CRUD/validation/FeignConfig.java
@@ -0,0 +1,9 @@
+package com.bosonit.Block7.CRUD.validation;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients
+public class FeignConfig {
+}
